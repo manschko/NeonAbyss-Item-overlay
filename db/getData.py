d9c21e75daea0c42f45ae38b7992b62a3a769819
manschko <manschko1999@googlemail.com>
2024-11-30 00:36:16 +0100

    feat(data retriever): update data retriever to reflect better data structure

--- db/getData.py
+++ db/getData.py
@@ -3,21 +3,52 @@ from bs4 import BeautifulSoup
 import json
 import os
 
+DOWNLOAD_IMAGES = False #just in case in need the images
+JSON_FILE = 'data.json'
+DICT_NAME = 'dict'
+# URL of the website
+URL = 'https://neonabyss.fandom.com/wiki/Items'
+
+def get_image_name(url):
+    name = url.split('.png')[0].split('/')[-1] + '.png'
+    name = name.replace('%27', "'")
+    name = name.lower()
+    name = name.replace('_', ' ')
+    return name
 
 # Function to download image
 def download_image(url, folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    response = requests.get(url)
-    image_name = url.split('.png')[0].split('/')[-1] + '.png'
-    with open(os.path.join(folder, image_name), 'wb') as file:
-        file.write(response.content)
+    image_name = get_image_name(url)
+    if DOWNLOAD_IMAGES:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        response = requests.get(url)
+        with open(os.path.join(folder, image_name), 'wb') as file:
+            file.write(response.content)
+
+
     return image_name[:-4]
 
 
+
+def create_dict(data):
+
+    # Create a set to store unique words
+    unique_words = set()
+
+    # Extract words from keys
+    for key in data.keys():
+        words = key.split(' ')
+        for word in words:
+            unique_words.add(word)
+
+    with open(DICT_NAME, 'w') as f:
+        for word in sorted(unique_words):
+            f.write(word + '\n')
+
 # Function to scrape table
-def scrape_table(url, folder):
-    response = requests.get(url)
+def scrape_table(folder):
+    response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     tables = soup.find_all('table')  # Adjust selector based on table's location
@@ -39,8 +70,9 @@ def scrape_table(url, folder):
     with open('data.json', 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
+    create_dict(data)
 
-# URL of the website
-url = 'https://neonabyss.fandom.com/wiki/Items'
-scrape_table(url, 'img')
+
+
+scrape_table('img')
 # Folder to
